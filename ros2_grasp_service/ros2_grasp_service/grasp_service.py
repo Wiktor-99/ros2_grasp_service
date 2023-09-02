b302826d491f110c57d9b74546cdf9a3cb73f4c7
Wiktor Bajor <wiktorbajor1@gmail.com>
2023-09-02 15:38:40 +0000

    Add refactor of reading parameters

--- ros2_grasp_service/ros2_grasp_service/grasp_service.py
+++ ros2_grasp_service/ros2_grasp_service/grasp_service.py
@@ -13,23 +13,16 @@ class GraspService(Node):
         self.declare_parameter('positions_to_home_postion_list', Parameter.Type.STRING_ARRAY)
         self.declare_parameter('time_to_wait_on_target', Parameter.Type.INTEGER)
 
-
-        self.positions_to_target_list = self.get_parameter('positions_to_target_list').get_parameter_value().string_array_value
         self.joints_names = self.get_parameter('joints_names').get_parameter_value().string_array_value
         self.time_to_wait_on_target = self.get_parameter('time_to_wait_on_target').get_parameter_value().integer_value
+        self.positions_to_target_list = self.get_parameter('positions_to_target_list').get_parameter_value().string_array_value
+        self.positions_to_home_postion_list = self.get_parameter('positions_to_home_postion_list').get_parameter_value().string_array_value
 
-        for position_to_target in self.positions_to_target_list:
-            self.declare_parameter(f'positions_to_target.{position_to_target}', Parameter.Type.DOUBLE_ARRAY)
-            self.declare_parameter(f'times_for_postions_to_target.{position_to_target}', Parameter.Type.DOUBLE)
+        self.declar_times_and_postions_parameters_from_list('times_for_postions_to_target', 'positions_to_target', self.positions_to_target_list)
+        self.declar_times_and_postions_parameters_from_list('times_for_postions_to_home_postion', 'positions_to_home_postion', self.positions_to_home_postion_list)
 
         self.positions_to_target = [ self.get_parameter(f'positions_to_target.{position_to_target}').get_parameter_value().double_array_value for position_to_target in self.positions_to_target_list]
         self.times_for_postions_to_target = [ self.get_parameter(f'times_for_postions_to_target.{position_to_target}').get_parameter_value().double_value for position_to_target in self.positions_to_target_list]
-
-        self.positions_to_home_postion_list = self.get_parameter('positions_to_home_postion_list').get_parameter_value().string_array_value
-
-        for position_to_home in self.positions_to_home_postion_list:
-            self.declare_parameter(f'positions_to_home_postion.{position_to_home}', Parameter.Type.DOUBLE_ARRAY)
-            self.declare_parameter(f'times_for_postions_to_home_postion.{position_to_home}', Parameter.Type.DOUBLE)
 
         self.positions_to_home = [ self.get_parameter(f'positions_to_home_postion.{position_to_target}').get_parameter_value().double_array_value for position_to_target in self.positions_to_home_postion_list]
         self.times_for_postions_to_home = [ self.get_parameter(f'times_for_postions_to_home_postion.{position_to_target}').get_parameter_value().double_value for position_to_target in self.positions_to_home_postion_list]
@@ -41,6 +34,12 @@ class GraspService(Node):
         self.get_logger().info(f'Got positions to home {self.positions_to_home}')
         self.get_logger().info(f'Got times for positions to home {self.times_for_postions_to_home}')
         self.get_logger().info(f'Got time to wait on target {self.time_to_wait_on_target}')
+
+    def declar_times_and_postions_parameters_from_list(self, times_prefix, position_prefix, list_of_parameters):
+        for position in list_of_parameters:
+            self.get_logger().info(f'{position_prefix}.{position}')
+            self.declare_parameter(f'{times_prefix}.{position}', Parameter.Type.DOUBLE)
+            self.declare_parameter(f'{position_prefix}.{position}', Parameter.Type.DOUBLE_ARRAY)
 
     def grasp(self, request, response):
         self.get_logger().info('Trigger received. Grasp sequence will be started.')
