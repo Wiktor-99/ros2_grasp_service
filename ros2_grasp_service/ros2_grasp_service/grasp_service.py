8be9138e408495436e0af4b9f3de568abb87a2e4
Wiktor Bajor <wiktorbajor1@gmail.com>
2023-09-03 11:00:10 +0000

    Change some naming

--- ros2_grasp_service/ros2_grasp_service/grasp_service.py
+++ ros2_grasp_service/ros2_grasp_service/grasp_service.py
@@ -10,22 +10,22 @@ class GraspService(Node):
         self.service = self.create_service(Empty, 'grasp_service', self.grasp)
         self.declare_parameter('joints_names', Parameter.Type.STRING_ARRAY)
         self.declare_parameter('positions_to_target_list', Parameter.Type.STRING_ARRAY)
-        self.declare_parameter('positions_to_home_postion_list', Parameter.Type.STRING_ARRAY)
+        self.declare_parameter('positions_to_home_list', Parameter.Type.STRING_ARRAY)
         self.declare_parameter('time_to_wait_on_target', Parameter.Type.INTEGER)
 
         self.joints_names = self.get_parameter('joints_names').get_parameter_value().string_array_value
         self.time_to_wait_on_target = self.get_parameter('time_to_wait_on_target').get_parameter_value().integer_value
         self.positions_to_target_list = self.get_parameter('positions_to_target_list').get_parameter_value().string_array_value
-        self.positions_to_home_postion_list = self.get_parameter('positions_to_home_postion_list').get_parameter_value().string_array_value
+        self.positions_to_home_list = self.get_parameter('positions_to_home_list').get_parameter_value().string_array_value
 
         self.declar_times_and_postions_parameters_from_list('times_for_postions_to_target', 'positions_to_target', self.positions_to_target_list)
-        self.declar_times_and_postions_parameters_from_list('times_for_postions_to_home_postion', 'positions_to_home_postion', self.positions_to_home_postion_list)
+        self.declar_times_and_postions_parameters_from_list('times_for_postions_to_home', 'positions_to_home', self.positions_to_home_list)
 
         self.positions_to_target = [ self.get_parameter(f'positions_to_target.{position_to_target}').get_parameter_value().double_array_value for position_to_target in self.positions_to_target_list]
         self.times_for_postions_to_target = [ self.get_parameter(f'times_for_postions_to_target.{position_to_target}').get_parameter_value().double_value for position_to_target in self.positions_to_target_list]
 
-        self.positions_to_home = [ self.get_parameter(f'positions_to_home_postion.{position_to_target}').get_parameter_value().double_array_value for position_to_target in self.positions_to_home_postion_list]
-        self.times_for_postions_to_home = [ self.get_parameter(f'times_for_postions_to_home_postion.{position_to_target}').get_parameter_value().double_value for position_to_target in self.positions_to_home_postion_list]
+        self.positions_to_home = [ self.get_parameter(f'positions_to_home.{position_to_target}').get_parameter_value().double_array_value for position_to_target in self.positions_to_home_list]
+        self.times_for_postions_to_home = [ self.get_parameter(f'times_for_postions_to_home.{position_to_target}').get_parameter_value().double_value for position_to_target in self.positions_to_home_list]
 
         self.log_parameters()
 
