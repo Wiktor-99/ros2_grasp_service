ffab94a9980419e04aeff240ea5d8153286ddde2
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-07-06 13:47:54 +0200

    Update flake8 config

--- .github/actions/lint/run.sh
+++ .github/actions/lint/run.sh
@@ -5,7 +5,7 @@ set -e
 
 if ["ament_flake8" == "ament_${LINTER}"]
 then
-    ament_flake8  . --config python_linter.flake8
+    ament_${LINTER} . --config python_linter.flake8
 else
     ament_${LINTER}
 fi
